5f75c9339900303d57bb0be7285b9f2fb277e3a0
Stefanos Valoumas <svaloumas@gmail.com>
2024-12-22 21:38:49 +0200

    refactor: export Result

--- job.go
+++ job.go
@@ -9,30 +9,30 @@ type Job interface {
 	Exec()
 }
 
-type result[T any] struct {
+type Result[T any] struct {
 	out T
 	err error
 }
 
-type task[Current, Previous any] func(previousResult result[Previous]) result[Current]
+type task[Current, Previous any] func(previousResult Result[Previous]) Result[Current]
 
 type job[Current, Previous any] struct {
 	task       task[Current, Previous]
-	resultChan chan result[Current]
+	resultChan chan Result[Current]
 	next       *job[Current, Previous]
 }
 
 func NewTask[Arg, Current, Previous any](ctx context.Context, args Arg, fn func(ctx context.Context, args Arg, previousResult result[Previous]) (Current, error)) task[Current, Previous] {
-	return func(previous result[Previous]) result[Current] {
+	return func(previous Result[Previous]) Result[Current] {
 		out, err := fn(ctx, args, previous)
-		return result[Current]{out: out, err: err}
+		return Result[Current]{out: out, err: err}
 	}
 }
 
 func NewJob[Current, Previous any](task task[Current, Previous]) *job[Current, Previous] {
 	return &job[Current, Previous]{
 		task:       task,
-		resultChan: make(chan result[Current], 1),
+		resultChan: make(chan Result[Current], 1),
 	}
 }
 
@@ -40,14 +40,14 @@ func (j *job[Current, Previous]) Link(next *job[Current, Previous]) {
 	j.next = next
 }
 
-func (j *job[Current, Previous]) Wait() <-chan result[Current] {
+func (j *job[Current, Previous]) Wait() <-chan Result[Current] {
 	return j.resultChan
 }
 
 func (j *job[Current, Previous]) Exec() {
 	var idx int = 1
-	var previous result[Previous]
-	var current result[Current]
+	var previous Result[Previous]
+	var current Result[Current]
 
 	current = j.task(previous)
 	if current.err != nil {
@@ -62,7 +62,7 @@ func (j *job[Current, Previous]) Exec() {
 		if !ok {
 			panic("damn...")
 		}
-		previous = result[Previous]{out: out, err: current.err}
+		previous = Result[Previous]{out: out, err: current.err}
 
 		current = j.next.task(previous)
 		if current.err != nil {
